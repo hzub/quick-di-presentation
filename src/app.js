4ed1972b18b428ebc90dfd55426ff14262fbf2ca
Hubert Zub <hubert.zub@gmail.com>
2017-02-25 17:38:22 +0100

    little console log

--- src/app.js
+++ src/app.js
@@ -10,8 +10,13 @@ Fiat.go();
 
 const DiFramework = require('./diFramework');
 
+console.info("[APP] Adding car...");
 DiFramework.use(require('./classes/car'));
+
+console.info("[APP] Adding engine...");
 DiFramework.use(require('./classes/engine'));
+
+console.info("[APP] Adding gearbox...");
 DiFramework.use(require('./classes/gearbox'));
 
 const myAppCar = DiFramework.getObject('Car');
