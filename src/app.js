c0937e2a5ade8f4e95e4a7362b7dfe5c759ba60d
Hubert Zub <hubert.zub@softwarehut.pl>
2017-03-09 09:03:28 +0100

    changes names to motherboard

--- src/app.js
+++ src/app.js
@@ -1,24 +1,14 @@
-/*
-const Car = require('./classes/car');
-const Engine = require('./classes/engine');
-const Gearbox = require('./classes/gearbox');
-
-const Fiat = new Car(new Engine(), new Gearbox());
-
-Fiat.go();
-*/
-
 const DiFramework = require('./diFramework');
 
-console.info("[APP] Adding car...");
-DiFramework.use(require('./classes/car'));
+console.info("[APP] Adding motherboard...");
+DiFramework.use(require('./classes/motherboard'));
 
-console.info("[APP] Adding engine...");
-DiFramework.use(require('./classes/engine'));
+console.info("[APP] Adding memory...");
+DiFramework.use(require('./classes/memory'));
 
-console.info("[APP] Adding gearbox...");
-DiFramework.use(require('./classes/gearbox'));
+console.info("[APP] Adding processor...");
+DiFramework.use(require('./classes/processor'));
 
-const myAppCar = DiFramework.getObject('Car');
+const myMotherboard = DiFramework.getObject('Motherboard');
 
-myAppCar.go();
+myMotherboard.start();
